6595dc37d53fec9b4afaf7cc95cce0c5fc1fef1f
mahomesishim <120954652+mahomesishim@users.noreply.github.com>
2024-03-27 17:42:04 -0400

    Update and rename uv_sw.js to uv.sw.js

--- uv_sw.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/uv_sw.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      console.error('ServiceWorker registration failed: ', err);
-    });
-  });
-}
